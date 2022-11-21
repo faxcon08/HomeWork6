32d24fdbdf803ddea536381ad939cfd03db9266c
faxcon08 <faxcon-08@mail.ru>
2022-11-21 16:07:03 +0300

    Second Task 1.2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        // First Task 1.1
+        System.out.println("First Task 1.1.");// First Task 1.1;
         for(int i = 1; i <=10; i++)
             System.out.print(i+" ");
         System.out.println();
+
+        System.out.println("Second Task 1.2");//Second Task 1.2;
+        for (int i =10; i>=1;i--)
+            System.out.print(i+" ");
+        System.out.println();
+
     }
 }
